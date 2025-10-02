77103ce60546550ba5047c45853aafc44bb3619b
aran8276 <aran8276@gmail.com>
2025-10-02 23:38:49 +0700

    feat: initiate a wwebjs project

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,50 @@
-import http from "node:http";
-import { PrismaClient } from "@prisma/client";
+import Whatsapp from "whatsapp-web.js";
+import qrcode from "qrcode-terminal";
 
-const prisma = new PrismaClient();
+const { Client, LocalAuth } = Whatsapp;
 
-const server = http.createServer(async (req, res) => {
-  try {
-    const users = await prisma.user.findMany();
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(users, null, 2));
-  } catch (e) {
-    console.error(e);
-    res.writeHead(500, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ error: "Failed to fetch users from database." }));
+process.on("uncaughtException", (err, origin) => {
+  console.error("----- Uncaught exception -----");
+  console.error(err);
+  console.error("----- Exception origin -----");
+  console.error(origin);
+  console.error("----- Bot will continue running -----");
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("----- Unhandled Rejection at -----");
+  console.error(promise);
+  console.error("----- Reason -----");
+  console.error(reason);
+  console.error("----- Bot will continue running -----");
+});
+
+const client = new Client({
+  authStrategy: new LocalAuth(),
+  webVersionCache: {
+    remotePath:
+      "https://raw.githubusercontent.com/wppconnect-team/wa-version/refs/heads/main/html/2.3000.1027891093-alpha",
+    type: "remote",
+  },
+  puppeteer: {
+    headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  },
+});
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+  console.log("Client is ready!");
+});
+
+client.on("message_create", (msg) => {
+  console.log(msg.body);
+  if (msg.body == "!ping") {
+    msg.reply("pong");
   }
 });
 
-server.listen(4000, () => {
-  console.log("Server running at http://localhost:4000/");
-});
+client.initialize();
---
+++ server/src/types.ts
@@ -0,0 +1,4 @@
+export interface ProcessCommand {
+  input: string;
+  output: () => void;
+}
