00e8967930c8948a74917fbd01d9569d3588a10e
Zahran Zaidan N <zahranzaidan.sekawanmedia@gmail.com>
2025-10-02 18:32:55 +0700

    feat: some meta icons and images

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { auth } from "@/lib/auth";
-
-export default auth;
-
-export const config = {
-  matcher: [
-    "/((?!api/auth/register|api/auth/signin|login|register|_next/static|_next/image|favicon.ico).*)",
-  ],
-};
